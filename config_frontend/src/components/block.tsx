8a5012e5a4f48c520331044dc16ca2e8a12a3389
antoniopataro <antoniopatarodev@gmail.com>
2023-09-13 10:01:14 -0300

    feat(config_frontend): adds block component

---
+++ config_frontend/src/components/block.tsx
@@ -0,0 +1,29 @@
+import React, { PropsWithChildren } from "react";
+
+import { Icon } from "@/components/icon";
+
+import { cn } from "@/lib/cn";
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  icon?: React.ComponentProps<typeof Icon>;
+}
+
+export const Block: React.FC<PropsWithChildren<Props>> = ({
+  children,
+  className,
+  icon,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex w-fit items-center gap-3 rounded-lg border border-dashed border-black/25 px-4 py-3 text-sm font-semibold text-black/75 transition-colors",
+        className,
+      )}
+      {...props}
+    >
+      {icon && <Icon {...icon} />}
+      {children}
+    </div>
+  );
+};
